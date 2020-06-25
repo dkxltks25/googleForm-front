3a376b391061b3a3b2034c3f96e2a2725dd39510
bagjaehong <dkxltks25@naver.com>
2020-06-26 02:10:47 +0900

    Drawer styiling complete

--- src/Component/Drawer.js
+++ src/Component/Drawer.js
@@ -1,14 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
@@ -18,69 +11,39 @@ const useStyles = makeStyles({
   drawerPaper: {
     width: 200,
     overflow: "auto",
+    height: "calc(100% - 106px)",
   },
 });
 
 export default () => {
-  //const classes = useStyles();
-  // const [state, setState] = React.useState({
-  //   right: false,
-  // });
-  // const toggleDrawer = (anchor, open) => (event) => {
-  //   if (
-  //     event.type === "keydown" &&
-  //     (event.key === "Tab" || event.key === "Shift")
-  //   ) {
-  //     return;
-  //   }
-  //   setState({ ...state, [anchor]: open });
-  // };
-
-  const list = (anchor) => {
-    return (
-      <div
-        role="presentation"
-        // onClick={toggleDrawer(anchor, false)}
-        // onKeyDown={toggleDrawer(anchor, false)}
-      >
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    );
-  };
   const classes = useStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
   return (
     <div>
-      {/* <Button onClick={toggleDrawer("right", true)}>right</Button> */}
+      <Button onClick={toggleDrawer("right", true)}>확인</Button>
       <Drawer
         anchor="right"
-        // open={state.right}
-        // onClose={toggleDrawer("right", false)}
+        open={state.right}
+        onClose={toggleDrawer("right", false)}
         className={classes.root}
-        variant="permanent"
+        variant="persistent"
       >
         <div className={classes.drawerPaper}>
-          <div style={{ height: 200 }}>123</div>
-          <div style={{ height: 200 }}>123</div>
+          <div style={{ height: 106 }} />
+          <Button onClick={toggleDrawer("right", false)}>닫기</Button>
+          <div style={{ height: 200 }}>1234</div>
           <div style={{ height: 200 }}>123</div>
           <div style={{ height: 200 }}>123</div>
           <div style={{ height: 200 }}>123</div>
